e63d13906f0470ac3f56e951796f125a5adf6abe
Chivas1717 <markgud1717@gmail.com>
2024-03-04 12:49:52 -0500

    fix: fixed missing param

--- lib/src/table_calendar.dart
+++ lib/src/table_calendar.dart
@@ -479,6 +479,7 @@ class _TableCalendarState<T> extends State<TableCalendar<T>> {
         Flexible(
           flex: widget.shouldFillViewport ? 1 : 0,
           child: TableCalendarBase(
+            timeSlots: [],
             onCalendarCreated: (pageController) {
               _pageController = pageController;
               widget.onCalendarCreated?.call(pageController);
