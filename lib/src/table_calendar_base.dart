dfb4293e2b9cf3d24473c48fefa95de62f74f707
Chivas1717 <markgud1717@gmail.com>
2024-05-31 12:12:17 -0400

    fix :hcanged row height

--- lib/src/table_calendar_base.dart
+++ lib/src/table_calendar_base.dart
@@ -331,7 +331,7 @@ class _TableCalendarBaseState extends State<TableCalendarBase> {
   double _getPageHeight(int rowCount, int activeRowCount) {
     final tablePaddingHeight = widget.tablePadding?.vertical ?? 0.0;
     final dowHeight = widget.dowVisible ? widget.dowHeight! : 0.0;
-    return dowHeight + rowCount * 38 + tablePaddingHeight + activeRowCount * 30;
+    return dowHeight + rowCount * 38 + tablePaddingHeight + activeRowCount * 38;
   }
 
   int _calculateFocusedPage(
